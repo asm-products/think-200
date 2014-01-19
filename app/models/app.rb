9be9eaee630fb20e4ae7d08cfc4f9ebff9f0c7ce
Robb Shecter <robb@weblaws.org>
2014-01-18 16:52:16 -0800

    Annotations

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: apps
+#
+#  created_at :datetime
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  project_id :integer
+#  updated_at :datetime
+#
+
 class App < ActiveRecord::Base
   belongs_to :project
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  created_at :datetime
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  updated_at :datetime
+#  user_id    :integer
+#
+
 class Project < ActiveRecord::Base
   has_many :apps
   belongs_to :user
