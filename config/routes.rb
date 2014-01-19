3d803261f30ddaa35f06430a94211e21e8c02afc
Robb Shecter <robb@weblaws.org>
2014-01-18 16:11:42 -0800

    User and Project models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Think200::Application.routes.draw do
+  resources :apps
+
+  resources :projects
+
   root "pages#home"    
   get "home", to: "pages#home", as: "home"
   get "inside", to: "pages#inside", as: "inside"
