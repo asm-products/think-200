09f301d0877d02daa09170e647e42aabef1a8faf
Robb Shecter <robb@weblaws.org>
2014-01-18 21:39:58 -0800

    docs

--- app/models/expectation.rb
+++ app/models/expectation.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: expectations
+#
+#  created_at     :datetime
+#  expectation    :string(255)
+#  id             :integer          not null, primary key
+#  matcher_id     :integer          not null
+#  requirement_id :integer          not null
+#  subject        :string(255)      not null
+#  updated_at     :datetime
+#
+
 class Expectation < ActiveRecord::Base
   belongs_to :requirement
   belongs_to :matcher
--- app/models/matcher.rb
+++ app/models/matcher.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: matchers
+#
+#  code       :string(255)      not null
+#  created_at :datetime
+#  id         :integer          not null, primary key
+#  max_args   :integer          not null
+#  min_args   :integer          not null
+#  updated_at :datetime
+#
+
 class Matcher < ActiveRecord::Base
 end
