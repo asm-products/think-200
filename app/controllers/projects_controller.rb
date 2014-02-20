5d1a7d0c1c6ba423d1ab1291281841c81e89ddb6
Robb Shecter <robb@weblaws.org>
2014-02-20 02:35:04 -0800

    docs

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,6 +13,8 @@ class ProjectsController < ApplicationController
 
   def export
     project = current_user.projects.find(params[:project_id])
+    # This is how GitHub does the "raw" file view. See headers, e.g.
+    # https://raw.github.com/dogweather/naturally/master/spec/naturally_spec.rb
     send_data project.to_rspec, 
       filename: project.name.gsub(' ', '_') + '_spec.rb', 
       type: 'text/plain',
