53dd532ddbbc6ab3452b76cb4cc39efc98b4cb05
Robb Shecter <robb@weblaws.org>
2014-03-03 13:15:24 -0800

    project index layout

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -31,7 +31,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    projects = current_user.projects
+    projects = current_user.projects.order(:name)
     @passed_projects =     projects.select{ |p| p.passed? }
     @failed_projects =     projects.select{ |p| p.passed? == false }
     @unfinished_projects = projects.select{ |p| p.passed?.nil? }
