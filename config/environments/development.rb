1df1aa09664292e78688b28fbff5b4704d877fe5
Robb Shecter <robb@weblaws.org>
2014-03-12 22:34:40 -0700

    cleanup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Think200::Application.configure do
   
   # Mailer
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:8997' }
+  config.action_mailer.default_url_options = { :host => 'devbox.srsly.co:8997' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:   'smtp.mandrillapp.com',
