50696f25ff97de46267f906b85c3f7ef54736d8d
Brandon CS Sanders <brandon@thesanders.us>
2014-03-16 21:14:07 -0700

    spec valid user/registrations#create

---
+++ spec/controllers/registrations_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require 'debugger'
+
+describe Users::RegistrationsController, '#create' do
+  let(:valid_create_params) do
+    {
+      user: {
+        username: "validname", 
+        email: "valid@email.com", 
+        password:              "sekret", 
+        password_confirmation: "sekret"
+      }
+    }
+  end
+
+  let(:session_with_stored_url) { {checkit_user_input: "stackoverflow.com"} }
+
+  describe "with valid params" do
+    it "creates the user" do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      expect {
+        post :create, valid_create_params, session_with_stored_url
+      }.to change{ User.count }.by(1)
+      expect(response.redirect_url).to eq('http://test.host/')
+    end
+
+    it "creates the project" do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      expect {
+        post :create, valid_create_params, session_with_stored_url
+      }.to change{ Project.count }.by(1)
+      expect(response.redirect_url).to eq('http://test.host/')
+    end
+  end
+end
