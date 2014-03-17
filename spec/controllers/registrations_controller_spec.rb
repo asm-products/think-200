c9b23b1cc04c6e4764a1de904967ede08b427512
Brandon CS Sanders <brandon@thesanders.us>
2014-03-16 21:15:29 -0700

    user/registrations#create handle errors

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -32,4 +32,17 @@ describe Users::RegistrationsController, '#create' do
       expect(response.redirect_url).to eq('http://test.host/')
     end
   end
+
+  describe "with an invalid user param" do
+    render_views
+
+    it "gives a good error message" do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      bad_create_params = valid_create_params.dup
+      bad_create_params[:user][:username] = 'bad username has spaces'
+      post :create, valid_create_params.merge( user: {username: 'bad username has spaces'} ), session_with_stored_url
+      expect(response).to render_template("new")
+      expect(response.body).to match /Username can only contain letters and digits/m
+    end
+  end
 end
