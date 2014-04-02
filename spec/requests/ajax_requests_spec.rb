09965a5a68ccb5adf3ca3a68219d7d55047c9c47
Robb Shecter <robb@weblaws.org>
2014-04-01 23:01:42 -0700

    Specs

---
+++ spec/requests/ajax_requests_spec.rb
@@ -0,0 +1,17 @@
+# require 'spec_helper'
+
+# describe 'The Ajax API' do
+#   before(:each) do
+#     @request.env["devise.mapping"] = Devise.mappings[:user]
+#     user = Fabricate.build(:user)
+#     user.confirm!
+#     sign_in user
+#   end
+
+#   describe "GET 'queue_status'" do
+#     it "returns http success for POST only" do
+#       get 'queue_status'
+#       response.should_not be_success
+#     end
+#   end
+# end
