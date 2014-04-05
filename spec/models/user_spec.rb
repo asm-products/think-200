e3aa19747c29eb4d056a95b251c619449cf4666f
Robb Shecter <robb@weblaws.org>
2014-04-05 00:10:04 -0700

    cleanup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,22 +36,21 @@
 require 'spec_helper'
 
 describe User do
-  let(:valid_user) {User.new email: 'foo@bar.com', password: 'sekret', username: 'iggy'}
-  let(:user)       {valid_user}
+  let(:user) { Fabricate.build(:user) }
 
   describe '#valid?' do
     it 'with valid attributes' do
-      expect(valid_user).to be_valid
+      expect(user).to be_valid
     end
 
     it 'when username <= 20' do
-      valid_user.username = 'x'*20
-      expect(valid_user).to be_valid
+      user.username = 'x'*20
+      expect(user).to be_valid
     end
 
     it 'when username >= 4' do
-      valid_user.username = 'x'*4
-      expect(valid_user).to be_valid
+      user.username = 'x'*4
+      expect(user).to be_valid
     end
   end
 
@@ -67,7 +66,7 @@ describe User do
     end
 
     it 'when username includes a space' do
-      user.username = 'Inigo Montoya'
+      user.username = 'Snack Time'
       expect(user).to be_invalid
     end
   end
