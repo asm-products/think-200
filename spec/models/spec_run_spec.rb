1ab81f64aeaaa18311caa5ece7aabaa9ac1a4ea2
Robb Shecter <robb@weblaws.org>
2014-04-07 15:06:39 -0700

    refactoring

--- spec/models/spec_run_spec.rb
+++ spec/models/spec_run_spec.rb
@@ -17,24 +17,27 @@
 require 'spec_helper'
 
 describe SpecRun do
+  let(:all_passed) { Fabricate.build(:spec_run_all_passed) }
+  let(:all_failed) { Fabricate.build(:spec_run_all_failed) }
+
   describe '#results' do
     it 'returns keys with Expectation ids' do
-      expect(Fabricate.build(:spec_run_all_passed).results.keys.sort).to eq [111, 222, 333]
+      expect(all_passed.results.keys.sort).to eq [111, 222, 333]
     end
 
     it 'notifies when a test passed' do
-      a_result = Fabricate.build(:spec_run_all_passed).results.values.first
+      a_result = all_passed.results.values.first
       expect(a_result.success?).to be true
     end
 
     it 'notifies when a test failed' do
-      a_result = Fabricate.build(:spec_run_all_failed).results.values.first
+      a_result = all_failed.results.values.first
       expect(a_result.success?).to be false
     end
   end
 
   describe '#covered?' do
-    subject { Fabricate.build(:spec_run_all_passed) } # Tested 111, 222, and 333
+    subject { all_passed } # Tested 111, 222, and 333
     specify { should be_covered([111, 222]) }
     specify { should be_covered([111, 222, 333]) }
     specify { should_not be_covered([111, 222, 999]) }
@@ -43,11 +46,11 @@ describe SpecRun do
 
   describe '#any_failed?' do
     it 'is true if all tests failed' do
-      expect( Fabricate.build(:spec_run_all_failed).any_failed? ).to be true
+      expect( all_failed.any_failed? ).to be true
     end
 
     it 'is false if all tests passed' do
-      Fabricate.build(:spec_run_all_passed).any_failed?.should be false
+      all_passed.any_failed?.should be false
     end
 
     it 'is true if some tests passed and some failed' do
@@ -57,7 +60,7 @@ describe SpecRun do
 
   describe '#passed?' do
     it 'is true when all tests passed' do
-      Fabricate.build(:spec_run_all_passed).passed?.should be true
+      all_passed.passed?.should be true
     end
 
     it 'is false if any test failed' do
@@ -67,38 +70,32 @@ describe SpecRun do
 
   describe '#exception_message_for' do
     it 'returns something when the exectation failed' do
-      spec_run = Fabricate.build(:spec_run_all_failed)
-      e        = Fabricate.build(:expectation, id: 888)
-      expect( spec_run.exception_message_for(expectation: e).length ).to be > 1
+      e = Fabricate.build(:expectation, id: 888)
+      expect( all_failed.exception_message_for(expectation: e).length ).to be > 1
     end
 
     it 'returns a blank string when the expectation passed' do
-      spec_run = Fabricate.build(:spec_run_all_passed)
-      e        = Fabricate.build(:expectation, id: 222)
-      expect( spec_run.exception_message_for(expectation: e).length ).to eql(0)
+      e = Fabricate.build(:expectation, id: 222)
+      expect( all_passed.exception_message_for(expectation: e).length ).to eql(0)
     end
 
     it 'returns a blank string when the expectation was not tested' do
-      spec_run = Fabricate.build(:spec_run_all_passed)
-      e        = Fabricate.build(:expectation, id: 1111111)
-      expect( spec_run.exception_message_for(expectation: e).length ).to eql(0)
+      e = Fabricate.build(:expectation, id: 1111111)
+      expect( all_passed.exception_message_for(expectation: e).length ).to eql(0)
     end
   end
 
   describe '#status?' do
     it "returns true if the expectation was tested and passed" do
-      spec_run = Fabricate.build(:spec_run_all_passed)
-      expect( spec_run.status? expectation: Fabricate.build(:expectation, id: 111) ).to be true
+      expect( all_passed.status? expectation: Fabricate.build(:expectation, id: 111) ).to be true
     end
 
     it "returns false if the expectation was tested and failed" do
-      spec_run = Fabricate.build(:spec_run_all_failed)
-      expect( spec_run.status? expectation: Fabricate.build(:expectation, id: 888) ).to be false
+      expect( all_failed.status? expectation: Fabricate.build(:expectation, id: 888) ).to be false
     end
 
     it "returns nil if the expectation was not tested" do
-      spec_run = Fabricate.build(:spec_run_all_passed)
-      expect( spec_run.status? expectation: Fabricate.build(:expectation, id: 999999) ).to be_nil
+      expect( all_passed.status? expectation: Fabricate.build(:expectation, id: 999999) ).to be_nil
     end
   end
 end
