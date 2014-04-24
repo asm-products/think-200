3d6a3f740c3e71d81391290e140642b0b50123ef
Robb Shecter <robb@weblaws.org>
2014-04-23 23:35:28 -0700

    Small cleanup

--- app/controllers/matchers_controller.rb
+++ app/controllers/matchers_controller.rb
@@ -1,15 +1,10 @@
 class MatchersController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_matcher, only: [:show]
 
-  # GET /matchers
-  # GET /matchers.json
   def index
     @matchers = Matcher.all
   end
 
-  # GET /matchers/1
-  # GET /matchers/1.json
   def show
   end
 end
